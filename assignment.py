f86ff7b04c21c6935f6de45ca4ade4bccffabbde
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-11 15:40:01 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -35,11 +35,21 @@ def solution(List):
 def cosineLaw(x,y,angle,oppositeSide = True):
     z = math.sqrt(float(x)**2 + float(y)**2 - 2*float(x)*float(y)*math.cos(convertAngle(angle)))
     if oppositeSide == False:
-        #if x = the oppositeSide
+        # c^2 = a^2 + b^2 - 2abcosC
+        # a^2 - 2abcosC + b^2 - c^2
+        # if x = the oppositeSide
         a = 1
         b = 2*float(y)*math.cos(convertAngle(angle))
         c = float(y)**2 - float(x)**2
-        z = solution(quadratic(a,b,c))
+        if float(b)**2 - 4*float(a)*float(c) >= 0:
+            z = solution(quadratic(a,b,c))
+        else:
+            # if y = the oppositeSide
+            a = 1
+            b = 2*float(x)*math.cos(convertAngle(angle))
+            c = float(x)**2 - float(y)**2
+            z = solution(quadratic(a,b,c))
+
     return z
 
 
